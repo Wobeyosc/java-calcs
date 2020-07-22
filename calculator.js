03af215ff3b4c99504bf51d8e2f61fbc5de2927d
Wobeyosc <47974672+Wobeyosc@users.noreply.github.com>
2020-07-22 22:30:01 +1000

    Updated function

---
+++ calculator.js
@@ -0,0 +1,6 @@
+function conversionRate (visitors, percentageConvert) {
+    percentageConvert = percentageConvert / 100;
+    return visitors * percentageConvert;
+}
+
+console.log(conversionRate(1000));
