efe22ebeafbe8001ef8b9c89ce998a348a964086
Wobeyosc <47974672+Wobeyosc@users.noreply.github.com>
2020-07-24 08:39:46 +1000

    Updated variables and to accept user input

--- calculator.js
+++ calculator.js
@@ -1,6 +1,15 @@
-function conversionRate (visitors, percentageConvert) {
-    percentageConvert = percentageConvert / 100;
-    return visitors * percentageConvert;
+let siteVisits = 1000
+let conversionPercent = 5
+
+siteVisits = prompt("Enter number of visitors", "1000");
+conversionPercent = prompt("Enter perecentage of visitors without % sign", "5");
+
+
+function conversionRate (visitors, percentage) {
+    percentage = percentage / 100;
+    return visitors * percentage;
 }
 
-console.log(conversionRate(1000, 5));
+result = conversionRate(siteVisits, conversionPercent);
+
+console.log(result + " Potential customers");
