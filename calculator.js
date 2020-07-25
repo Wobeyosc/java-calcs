ebe068e5b5bddc7d319bdc4d0bde80e0e8efb8c9
Wobeyosc <47974672+Wobeyosc@users.noreply.github.com>
2020-07-25 12:21:43 +1000

    Updated steps to be in a function

--- calculator.js
+++ calculator.js
@@ -1,17 +1,17 @@
-
 let siteVisits = 1000;
 let conversionPercent = 10;
-siteVisits = document.getElementById('numVisit').value;
-conversionPercent = document.getElementById('rate').value;
-
 
 function conversionRate (visitors, percentage) {
     percentage = percentage / 100;
     return visitors * percentage;
 }
 
-result = conversionRate(siteVisits, conversionPercent);
+function formValues () {
+    siteVisits = document.getElementById('numVisit').value;
+    conversionPercent = document.getElementById('rate').value;
+    result = conversionRate(siteVisits, conversionPercent);
+    console.log("If you have " + siteVisits + " site visits a month and " + conversionPercent + "% conversion then you have");
+    console.log(result + " Potential customers");
+}
 
-console.log("If you have " + siteVisits + " site visits a month and " + conversionPercent + "% conversion then you have");
 
-console.log(result + " Potential customers");
