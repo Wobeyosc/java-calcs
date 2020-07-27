04ede1308c1a404e1573c4f85a710648338eacdf
Wobeyosc <47974672+Wobeyosc@users.noreply.github.com>
2020-07-27 20:54:48 +1000

    updated javascript background color to white

--- calculator.js
+++ calculator.js
@@ -20,7 +20,7 @@ function updatePage () {
     document.getElementById("convertID").innerHTML = conversionPercent;
     document.getElementById("customerResult").innerHTML = result;
     document.getElementById("results").style.color = "black";
-    document.getElementById("results").style.backgroundColor = "aliceblue";
+    document.getElementById("results").style.backgroundColor = "white";
     document.getElementById("visitors").style.color = "#0249a5";
     document.getElementById("convertID").style.color = "#0249a5";
     document.getElementById("customerResult").style.color = "#0249a5";
